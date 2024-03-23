5120d610e81de243e8318bcaed40f135feb6ba00
Pedro Afonso Silva Fanica <90769037+PFans-201@users.noreply.github.com>
2024-03-23 22:45:52 +0000

    Delete nlpaug_rephraser.py

--- nlpaug_rephraser.py
+++
@@ -1,8 +0,0 @@
-import nlpaug.augmenter.word as naw
-
-class NLPAugRephraser:
-    def __init__(self):
-        self.aug = naw.SynonymAug(aug_src='wordnet')
-
-    def rephrase(self, text):
-        return self.aug.augment(text)
