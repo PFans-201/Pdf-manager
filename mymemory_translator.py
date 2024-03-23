1e58b3fb22681740f3685fb9976cdee22dc9454b
Pedro Afonso Silva Fanica <90769037+PFans-201@users.noreply.github.com>
2024-03-23 22:45:42 +0000

    Delete mymemory_translator.py

--- mymemory_translator.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-class MyMemoryTranslator:
-    def translate(self, text, source_lang, target_lang):
-        url = f"https://api.mymemory.translated.net/get?q={text}&langpair={source_lang}|{target_lang}"
-        response = requests.get(url)
-        data = response.json()
-        return data['responseData']['translatedText']
